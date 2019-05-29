4426bcbc5b1a6a2f64fb74255f04da0c5300b91b
Mike Rathbun <mikerathbun@me.com>
2019-05-28 22:11:59 -0500

    basic react working

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-
+class ProductList extends React.Component {
+    render() {
+        return (
+            <div className='ui unstackable items'>
+            Hello This is a basic React component.
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
